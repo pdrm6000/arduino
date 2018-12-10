b98b08745174e90c0067b8db3e495a0e6e9877f2
pdrm6000 <pdrm6000@gmail.com>
2018-12-10 16:55:50 +0100

    new version flashed to arduino

--- main/main.ino
+++ main/main.ino
@@ -10,6 +10,7 @@
 Adafruit_SSD1306 display(OLED_RESET);
 // Configure STH30 sensor I2C address
 SHT3X sht30(0x45);
+int sensorToPrint = 1;
 
 void setup() {
 
@@ -40,31 +41,48 @@ void clearDisplayBuffer() {
   // Clear the buffer.
   display.clearDisplay();
   display.setTextSize(1);
+  display.setRotation(1);
   display.setCursor(0, 0);
   display.setTextColor(WHITE);
 }
 
 void printSensorsMetricsToOLEDDisplay() {
-  int sensorValue = analogRead(32);
   
-  display.setTextSize(1);
-  display.println("T: ");
+  float sensorValue1 = analogRead(32) / 100.0f;
+  float sensorValue2 = analogRead(33) / 100.0f;
+  float sensorValue3 = analogRead(34) / 100.0f;
+
+  char buf[7];
+  snprintf (buf, 7, "Sen. %d:", sensorToPrint);
+
+  display.println(buf);
   display.setTextSize(2);
-  //display.println(sht30.cTemp);
-  display.println(sensorValue);
+  if (sensorToPrint == 1)
+    display.println(sensorValue1, 1);
+  else if (sensorToPrint == 2)
+    display.println(sensorValue2, 1);
+  else
+  {
+    display.println(sensorValue3, 1);
+    sensorToPrint = 0;
+  }
+  sensorToPrint++;
 
   display.setTextSize(1);
-  display.println("H: ");
-  display.setTextSize(1);
+  display.println("");
+  display.print("T: ");
+  display.println(sht30.cTemp);
+  display.print("H: ");
   display.println(sht30.humidity);
 
-  
+  /*
+   * need to deal with this later
   if (timeStatus() != timeSet)
   {
     display.setTextSize(1);
     display.print("Not sending!!!");
     display.setTextSize(1);
   }
-  
+  */
   
 }
